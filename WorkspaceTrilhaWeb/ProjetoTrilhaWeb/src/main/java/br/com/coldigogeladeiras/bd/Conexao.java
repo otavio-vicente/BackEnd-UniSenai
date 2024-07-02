724034283cfbcf01dd0d9330e158c7a74500baad
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2024-07-02 19:53:19 -0300

    Finalização OT17 BackEnd

--- WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/bd/Conexao.java
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/bd/Conexao.java
@@ -6,4 +6,24 @@ public class Conexao {
 
 	private Connection conexao;
 	
+	public Connection abrirConexao() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			conexao = java.sql.DriverManager
+					.getConnection("jdbc:mysql://localhost/bdcoldigo?"
+					+ "user=root&password=root&useTimezone=true&serverTimezone=UTC");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conexao;
+	}
+	
+	public void fecharConexao() {
+		try {
+			conexao.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
---
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/jdbc/JDBCMarcaDAO.java
@@ -0,0 +1,68 @@
+package br.com.coldigogeladeiras.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import java.util.ArrayList;
+
+import br.com.coldigogeladeiras.jdbcinterface.MarcaDAO;
+import br.com.coldigogeladeiras.modelo.Marca;
+
+public class JDBCMarcaDAO implements MarcaDAO {
+
+	private Connection conexao;
+	
+	public JDBCMarcaDAO(Connection conexao) {
+		this.conexao = conexao;
+	}
+	
+	public List<Marca> buscar() {
+		
+		//Criação da instrução SQL para busca de todas as marcas;
+		String comando = "SELECT * FROM marcas";
+		
+		//Criação de uma lista para armazenar cada marca encontradas;
+		List<Marca> listMarcas = new ArrayList<Marca>();
+		
+		//Criação do objeto marca com valor null (ou seja, sem instanciá-lo)
+		Marca marca = null;
+		
+		try {
+			
+			//Uso da conexão do banco de dados para prepara-lo para uma instrução SQL
+			Statement stmt = conexao.createStatement();
+			//Execução da instrução criada previamente e armazenamento do resultado do objeto rs
+			ResultSet rs = stmt.executeQuery(comando);
+			
+			while(rs.next()) {
+				
+				//Criação de instância da classe
+				marca = new Marca();
+				
+				//Recebimento dos 2 dados retornados do BD para cada linha encontrada
+				int id = rs.getInt("id");
+				String nome = rs.getString("nome");
+				
+				//Setando no objeto marca os valores encontrados
+				marca.setId(id);
+				marca.setNome(nome);
+				
+				//Adição da instância contida no objeto Marca na lista de marcas
+				listMarcas.add(marca);
+				
+			}
+			
+			
+				
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+		
+		//retorna para quem chamou o método a lista criada
+		return listMarcas;
+	}
+}
